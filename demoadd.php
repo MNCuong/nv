f78cda7247dcce367bd09887f2cb153fec589a0a
MNCuong <100787138+MNCuong@users.noreply.github.com>
2023-08-28 04:03:38 +0700

    Create demoadd.php

---
+++ demoadd.php
@@ -0,0 +1,208 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Add customer</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<script src="js/bootstrap.js"></script>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+</head>
+
+<body style="background: linear-gradient(0.7turn, #ebf8e1, #f69d3c);" >
+	
+	
+	
+		<div class="row1" >
+			<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top " style="box-shadow: 0 0 50px black ">
+   <a class="navbar-brand" href="#"><strong>Quản lý nhân viên</strong></a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+   <span class="navbar-toggler-icon"></span>
+   </button>
+   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+         <li class="nav-item ">
+            <a class="nav-link" href="#">Nhân viên <span class="sr-only">(current)</span></a>
+         </li>
+         <li class="nav-item">
+            <a class="nav-link" href="#">Điểm danh</a>
+         </li>
+         <li class="nav-item">
+            <a class="nav-link" href="#">Tiền lương</a>
+         </li>
+		  
+
+		  <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="list" data-bs-toggle="dropdown">
+            Tài khoản
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li> <a class="dropdown-item" href="#">Sửa tài khoản</a></li>
+               <li><a class="dropdown-item" href="#">Xóa tài khoản</a></li>
+               <li><div class="dropdown-divider"></div></li>
+               <li><a class="dropdown-item" href="#">Khác</a></li>
+            </ul>
+         </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+   </div>
+</nav>
+		</div>
+
+		<div class="row2">
+			<div class="container">
+			<p style="padding-top: 100px" class="col-sm-9 w-100 col-xl-8 ">Tìm kiếm nhân viên</p>
+			<div id="clock"><i class="fa fa-clock" aria-hidden="true"></i> <span class="date">Thứ Hai, 28/08/2023 | 2:52:34</span></div>
+				
+			<input type="search" name="" class="form-control mt-2" id="" border="none" placeholder="Tìm kiếm">
+
+		</div>
+		</div>
+	
+		<div class="row3 mt-5">
+			<table class="table table-light">
+			  <thead class="table-dark">
+				<tr>
+				  	<th scope="col">STT</th>
+				  	<th scope="col">Họ tên</th>
+				  	<th scope="col">Giới tính</th>
+				  	<th scope="col">Ngày sinh</th>
+					<th scope="col">Địa chỉ</th>
+					<th scope="col">Chức vụ</th>
+					<th scope="col">Tính năng</th>
+				</tr>
+			  </thead>
+			  <tbody>
+				<tr>
+				  	<th scope="row">1</th>
+				  	<td>Mai Ngọc Cường</td>
+				 	<td>Nam</td>
+				  	<td>09/01/2003</td>
+					<td>Thanh Hóa</td>
+					<td>Nhân viên</td>
+					<td>
+						<i class="fa-solid fa-pen"></i>/
+						<i class="fa-solid fa-trash " ></i>
+					</td>
+				</tr>
+				<tr>
+				  <th scope="row">2</th>
+				  <td>Lưu Thị Hạnh</td>
+				  <td>Nữ</td>
+				  <td>...</td>
+					<td>Vĩnh Phúc</td>
+					<td>Nhân viên</td>
+					<td>
+						<i class="fa-solid fa-pen"></i>/
+						<i class="fa-solid fa-trash "></i>
+					</td>
+				</tr>
+				<tr>
+				 	<th scope="row">3</th>
+				 	<td >Nguyễn Thị Nga</td>
+				  	<td>Nữ</td>
+					<td>23/08/2003</td>
+					<td>Thanh Hóa</td>
+					<td>Nhân viên</td>
+					<td>
+						<i class="fa-solid fa-pen"></i>/
+						<i class="fa-solid fa-trash "></i>
+					</td>
+				</tr>
+				<tr>
+				 	<th scope="row">4</th>
+				 	<td >Nguyễn Thị Nga</td>
+				  	<td>Nữ</td>
+					<td>23/08/2003</td>
+					<td>Thanh Hóa</td>
+					<td>Nhân viên</td>
+					<td>
+						<i class="fa-solid fa-pen"></i>/
+						<i class="fa-solid fa-trash "></i>
+					</td>
+				</tr>
+				<tr>
+				 	<th scope="row">5</th>
+				 	<td >Nguyễn Thị Nga</td>
+				  	<td>Nữ</td>
+					<td>23/08/2003</td>
+					<td>Thanh Hóa</td>
+					<td>Nhân viên</td>
+					<td>
+						<i class="fa-solid fa-pen"></i>/
+						<i class="fa-solid fa-trash "></i>
+					</td>
+				</tr>
+				<tr>
+				 	<th scope="row">6</th>
+				 	<td >Nguyễn Thị Nga</td>
+				  	<td>Nữ</td>
+					<td>23/08/2003</td>
+					<td>Thanh Hóa</td>
+					<td>Nhân viên</td>
+					<td>
+						<i class="fa-solid fa-pen"></i>/
+						<i class="fa-solid fa-trash "></i>
+					</td>
+				</tr>
+				<tr>
+				 	<th scope="row">7</th>
+				 	<td >Nguyễn Thị Nga</td>
+				  	<td>Nữ</td>
+					<td>23/08/2003</td>
+					<td>Thanh Hóa</td>
+					<td>Nhân viên</td>
+					<td>
+						<i class="fa-solid fa-pen"></i>/
+						<i class="fa-solid fa-trash "></i>
+					</td>
+				</tr>
+				<tr>
+				 	<th scope="row">8</th>
+				 	<td >Nguyễn Thị Nga</td>
+				  	<td>Nữ</td>
+					<td>23/08/2003</td>
+					<td>Thanh Hóa</td>
+					<td>Nhân viên</td>
+					<td>
+						<i class="fa-solid fa-pen"></i>/
+						<i class="fa-solid fa-trash "></i>
+					</td>
+				</tr>
+				
+			  </tbody>
+			</table>
+		</div>
+		
+	
+	
+		
+		<div class="row mt-5 " style="text-align: center; ">
+		  <div class="col-lg-4" >Thông tin liên lạc</div>
+			<div class="col-lg-4">Danh mục</div>
+			<div class="col-lg-4">Hỗ trợ</div>
+		</div>
+
+
+		
+		<div class="row-end text-center mt-5" style="font-size: 30px; color: black">
+			<a style=" color: black" href="https://www.facebook.com/MNC3000"><i class="fa-brands fa-facebook"></i></a>
+			<a style=" color: black" href="https://www.instagram.com/meikun.01?fbclid=IwAR38FR19coC_iYdr3POIHCf2ehT8zMrSMhRIZPE2Emb6TYBR1dEmpWmbJcE"><i class="fa-brands fa-instagram"></i></a>
+			<a style=" color: black" href="https://github.com/MNCuong?fbclid=IwAR2PbDkO_u8ofCrojCYePupHR9Igl-9sO9pfQQj3ZJahqPaEYHyFdVt-Vsc"><i class="fa-brands fa-github"></i></a>
+			<a style=" color: black" href="https://www.tiktok.com/@cuon912"><i class="fa-brands fa-tiktok"></i></a>
+			
+			
+			
+			
+			
+		</div>
+
+	</div>	
+		
+
+	
+</body>
+</html>
